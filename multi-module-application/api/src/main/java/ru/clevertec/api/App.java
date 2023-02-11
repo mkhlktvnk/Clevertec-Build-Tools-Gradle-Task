08fcfd7b308d98fdc74ca8c9582bbd7c8e0fb097
misha <mikhail.kutovenko@gmail.com>
2023-02-12 00:26:44 +0300

    implement App

--- multi-module-application/api/src/main/java/ru/clevertec/api/App.java
+++ multi-module-application/api/src/main/java/ru/clevertec/api/App.java
@@ -1,2 +1,9 @@
-package ru.clevertec.api;public class App {
+package ru.clevertec.api;
+
+import ru.clevertec.core.Utils;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
+    }
 }
