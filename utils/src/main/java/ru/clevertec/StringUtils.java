dee4f15e256fc7cdbed5e9347738b51758744e72
misha <mikhail.kutovenko@gmail.com>
2023-02-11 22:36:10 +0300

    implement method isPositiveNumber()

--- utils/src/main/java/ru/clevertec/StringUtils.java
+++ utils/src/main/java/ru/clevertec/StringUtils.java
@@ -1,2 +1,7 @@
-package ru.clevertec;public class StringUtils {
+package ru.clevertec;
+
+public class StringUtils {
+    public static boolean isPositiveNumber(String str) {
+        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Integer.parseInt(str) > 0;
+    }
 }
