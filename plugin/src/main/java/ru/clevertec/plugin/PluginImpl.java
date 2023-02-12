436f38d481328d3346a538e73f64d89faa09e5c6
misha <mikhail.kutovenko@gmail.com>
2023-02-12 19:15:05 +0300

    implement PluginImpl

--- plugin/src/main/java/ru/clevertec/plugin/PluginImpl.java
+++ plugin/src/main/java/ru/clevertec/plugin/PluginImpl.java
@@ -1,4 +1,11 @@
 package ru.clevertec.plugin;
 
-public class Plugin {
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class PluginImpl implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        project.getTasks().create("pluginTask", Task.class);
+    }
 }
