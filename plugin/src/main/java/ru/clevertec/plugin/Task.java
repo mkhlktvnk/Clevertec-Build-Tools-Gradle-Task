d4581acee020d44edb22a7959c39c4676126e114
misha <mikhail.kutovenko@gmail.com>
2023-02-12 19:09:30 +0300

    implement Task

--- plugin/src/main/java/ru/clevertec/plugin/Task.java
+++ plugin/src/main/java/ru/clevertec/plugin/Task.java
@@ -1,2 +1,11 @@
-package ru.clevertec.plugin;public class Task {
+package ru.clevertec.plugin;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class Task extends DefaultTask {
+    @TaskAction
+    public void executeTask() {
+        System.out.println("Task executed!");
+    }
 }
